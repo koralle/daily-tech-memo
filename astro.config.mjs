9f0bf804a77fb8c3e350572d52d54d5a140cb2b7
koralle <mkhd7930@gmail.com>
2024-02-04 23:49:41 +0900

    chore(Astro): integrated `Starlight`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare()
+	output: "server",
+	adapter: cloudflare(),
+	integrations: [starlight()],
 });
